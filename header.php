72750e51ce535c1e2fe4646bd1f0657ea3b1aa22
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-11-26 16:19:13 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,30 +1,30 @@
-<!doctype html>
-<html lang="en">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Homework 6</title>
-  </head>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="google.php">Google Graph</a>
+    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">D3 Scatter Plot</a>
+          <a class="nav-link active" aria-current="page" href="Three.js.html">function1</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="plotly.php">Plotly</a>
+          <a class="nav-link" href="D3.js.html">function2</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="chart.php">Chart.js</a>
+          <a class="nav-link" href="chart.js.html">function3</a>
+        </li>
+	       <li class="nav-item">
+          <a class="nav-link" href="google-charts.html">function4</a>
+        </li> 
+	      <li class="nav-item">
+          <a class="nav-link" href="plotly.html">function5</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
-<body>
