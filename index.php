71c7307973830dcf43382c334b8ba023a38573d2
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-11-29 21:05:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     
     <body style="background-color:powderblue;">
-      <h1>Sana Yari's Homework 6</h1>
-      <p>JavaScript Libraries</p>
+      <h1 style="text-align:center;">Sana Yari's Homework 6</h1>
+      <p style="text-align:center;">JavaScript Libraries</p>
     </body>
   
   </head>
