de7e14334c24d7a802d33fbc0a547136ba7fd0fd
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-11-29 20:24:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php require_once("header.php"); ?>
 
 <!doctype html>
@@ -6,12 +5,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>HomePage HW 6</title>
+    <title>Sana Yari's HW 6</title>
     
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  
-  
   
   </head>
   <body>
